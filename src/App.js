2703fee8da460184549f1afde768d2576fd8fa0d
David Tobasura <hollerller@gmail.com>
2025-06-11 00:24:37 -0500

    Initial structure with React and Tailwind

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//import logo from './logo.svg';
+import './App.css';
+
+import React from "react";
+
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+//import Experiences from "./components/Experiences";
+
+
+function App() {
+  return (
+<main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
